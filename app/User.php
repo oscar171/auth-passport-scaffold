3d6da33d14b0476167d9bd59b055b805a4cdf975
oscar171 <oscarp171@gmail.com>
2020-05-30 17:30:17 -0400

    first commit

---
+++ app/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Storage;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens,Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password','active', 'activation_token','avatar',
+    ];
+
+    protected $dates = ['deleted_at'];
+    protected $appends = ['avatar_url'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token','activation_token'
+    ];
+
+    public function getAvatarUrlAttribute()
+    {
+        return Storage::url('avatars/'.$this->id.$this->avatar);
+    }
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
